ae83ab1b1fb6119dd7d96c650bc8aa6868bf028c
Hardware-Man <km1233@scarletmail.rutgers.edu>
2019-11-20 20:28:17 -0500

    Add files via upload

--- basicmotor/basicmotor.ino
+++ basicmotor/basicmotor.ino
@@ -18,26 +18,29 @@
 #define stby 6
 
 /*Circumference of the wheels (based on measured diameter value)*/
-#define CIRC 3.14159265359*40.2
+const double CIRC = 3.14159265359*40.2;
 
 /*aCount and bCount measure the number of ticks that the encoder sensors have counted*/
-//The number of ticks in 1 rotation are 180
+//The number of ticks in 1 rotation are 360
 volatile int aCount = 0;
 volatile int bCount = 0;
 
 /*For my code, I want motor B to try and match up to motor A's speed, so I will make motor B's speed subject to change*/
-//Initially, bSpeed = 100, but this will change as the PID accounts for the difference between motor B's and motor A's speeds
-int bSpeed = 50;
+//Initially, bSpeed = 40, but this will change as the PID accounts for the difference between motor B's and motor A's speeds
+int bSpeed = 40;
 
 /*Here is the PID setup to work with (read the library online for more details)*/
 double Input = 0.0, Output = 0.0, Setpoint = 0.0;
 /*You can change the constants for Kp, Ki, and Kd based on your testing results*/
-double Kp = 4, Ki = 12, Kd = 0.2;
-
+double Kp = 2, Ki = 4, Kd = 0.1;
+/*Create the PID controller*/
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
-/*Changes aCount based on the spinning of motor A*/
-void aSpin() {
+/*
+  Changes bCount based on the spinning of motor B
+  Remember to read both channels for full tick count!
+*/
+void aASpin() {
   if (digitalRead(aeoa) == digitalRead(aeob)) {
     aCount--;
   }
@@ -46,13 +49,34 @@ void aSpin() {
   }
 }
 
-/*Changes bCount based on the spinning of motor B*/
-void bSpin() {
+void aBSpin() {
+  if (digitalRead(aeob) == digitalRead(aeoa)) {
+    aCount++;
+  }
+  else {
+    aCount--;
+  }
+}
+
+/*
+  Changes bCount based on the spinning of motor B
+  Remember to read both channels for full tick count!
+*/
+void bASpin() {
   if (digitalRead(beoa) == digitalRead(beob)) {
     bCount--;
   }
   else {
     bCount++;
+  }
+}
+
+void bBSpin() {
+  if (digitalRead(beob) == digitalRead(beoa)) {
+    bCount++;
+  }
+  else {
+    bCount--;
   }
 }
 
@@ -63,8 +87,10 @@ void moveForwardOne() {
   digitalWrite(bin2, LOW);
 }
 
+#define blueserial Serial1
+
 void setup() {
-  Serial1.begin(38400);
+  blueserial.begin(38400);
   
   //Set up pin modes based on whether you're reading or sending values
   pinMode(ain1, OUTPUT);
@@ -80,7 +106,7 @@ void setup() {
   pinMode(stby, OUTPUT);
 
   //Remember to set STBY to high, otherwise your bot won't run
-  digitalWrite(stby, HIGH);
+  digitalWrite(stby, LOW);
 
   //Here, you're setting the initial direction and speed your motors are gonna go
   digitalWrite(ain1, LOW);
@@ -91,16 +117,49 @@ void setup() {
   analogWrite(pwmb, bSpeed);
 
   //The aSpin and bSpin functions will be called each time there is a change in the values of the encoders' A channels
-  attachInterrupt(digitalPinToInterrupt(aeoa), aSpin, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(beoa), bSpin, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(aeoa), aASpin, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(aeob), aBSpin, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(beoa), bASpin, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(beob), bBSpin, CHANGE);
 
   //Turn the PID on by having it's mode set to AUTOMATIC
   myPID.SetMode(AUTOMATIC);
-  myPID.SetSampleTime(10);
-//  myPID.SetOutputLimits(0,60);
+  myPID.SetSampleTime(1);
 }
 
+bool pass = false;
 void loop() {
+  if(blueserial.available()){
+    Serial.write(blueserial.read());
+  }
+  if(Serial.available()){
+    blueserial.write(Serial.read());
+  }
+}
+
+/*
+  if(millis() < 2000){
+    return;
+  }
+  if(!pass){
+    digitalWrite(stby, HIGH);
+    digitalWrite(bin1, HIGH);
+    digitalWrite(bin2, LOW);
+    myPID.SetTunings(0.5,5,0.1);
+    myPID.SetSampleTime(0.1);
+    while(aCount < 90){
+      Input = bCount-aCount;
+      myPID.Compute();
+      bSpeed = Output;
+      analogWrite(pwmb, bSpeed);
+      Serial.println(Output);
+    }
+    digitalWrite(stby, LOW);
+    pass = true;
+  }
+*/
+
+/*
   if(millis() < 2000){
     digitalWrite(stby, LOW);
     return;
@@ -122,5 +181,5 @@ void loop() {
   //To check it's working, keep track of the input and setpoint as your bot runs
   //The input should go towards the setpoint and hover around it, indicating that the difference in your motors' tick counts are around 0
   Serial.println(Input);
-  delay(1);
-}
+  delay(10);
+*/
